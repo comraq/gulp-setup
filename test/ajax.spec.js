91ea6bd81d28b4567cc6693a8bb6e57c27d273e7
comraq <yinadam@hotmail.com>
2016-05-17 13:14:17 -0400

    updating ajax util and tests

--- test/ajax.spec.js
+++ test/ajax.spec.js
@@ -7,15 +7,17 @@ describe("utils: ", () => {
     after(() => server.restore());
 
     it("should GET data", done => {
-      ajax("GET", "/")
-        .then(res => {
-          res = JSON.parse(res);
-          res.data.should
-            .have.property("some key", "some value");
+      ajax({
+        method: "GET",
+        path: "/"
+      }).then(res => {
+        res = JSON.parse(res);
+        res.data.should
+          .have.property("some key", "some value");
 
-          done();
-        })
-        .catch(err => done(err));
+        done();
+      })
+      .catch(err => done(err));
     
       server.requests[0].respond(
         200,
@@ -24,6 +26,30 @@ describe("utils: ", () => {
           "some key": "some value",
           "another key": "another value"
         })
+      );
+    });
+
+    it("should POST data", done => {
+      ajax({
+        method: "POST",
+        path: "/",
+        body: {
+          postkey: "post value"
+        }
+      }).then(res => {
+        res = JSON.parse(res);
+        expect(res.data.message).to.deep.equal("Success!");
+        done();
+      })
+      .catch(done)
+    
+      let req = server.requests[1];
+      req.requestBody.should.have.property("postkey", "post value");
+
+      req.respond(
+        200,
+        { "Content-Type": "application/json" },
+        JSON.stringify({ message: "Success!" })
       );
     });
   });
